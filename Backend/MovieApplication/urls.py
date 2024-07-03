3eca9880e0aacd91e7071f79371c6ca4d77d8c7a
Fourat Gazzeh <fourat.gazzeh@gmail.com>
2024-07-03 01:23:14 +0100

    setup

--- Backend/MovieApplication/urls.py
+++ Backend/MovieApplication/urls.py
@@ -1,7 +1,8 @@
-"""MovieApplication URL Configuration
+"""
+URL configuration for MovieApplication project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,8 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
+     #path('admin/', admin.site.urls)
     path('api/', include('recommandersystem.urls')),
 ]
--- Backend/recommandersystem/models.py
+++ Backend/recommandersystem/models.py
@@ -10,23 +10,3 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-class Movie(models.Model):
-        id = models.AutoField(primary_key=True)
-        title = models.CharField(max_length=255)
-        description = models.TextField()
-        release_date = models.DateField()
-
-        def __str__(self):
-            return self.title
-
-class Favorite(models.Model):
-    id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='favorited_by')
-    added_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ('user', 'movie')
-
-    def __str__(self):
-        return f'{self.user.email} - {self.movie.title}'
